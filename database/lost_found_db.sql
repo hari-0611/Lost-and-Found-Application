0701d226a50432c70039db4a498e1f34b7bc0d3f
Hari Charan <khck6112004@gmail.com>
2025-06-23 19:53:52 +0530

    Added required sql queries for creation of required tables

--- database/lost_found_db.sql
+++ database/lost_found_db.sql
@@ -1,32 +1,28 @@
 -- Creation of database
-
 CREATE DATABASE IF NOT EXISTS lost_found_db;
 USE lost_found_db;
 
--- Lost items table with name items
-
-CREATE TABLE IF NOT EXISTS items (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  itemName VARCHAR(100),
-  category VARCHAR(50),
-  description TEXT,
-  lastSeenLocation VARCHAR(255),
-  dateLost DATE,
-  contactInfo VARCHAR(100),
-  imageUrl VARCHAR(255)
+-- Lost items table
+CREATE TABLE lost_items (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    itemName VARCHAR(255) NOT NULL,
+    category VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    lastSeenLocation VARCHAR(255) NOT NULL,
+    dateLost DATE NOT NULL,
+    contactInfo VARCHAR(255) NOT NULL,
+    imageUrl VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    username VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-ALTER TABLE items
-ADD COLUMN phone_number VARCHAR(15),
-ADD COLUMN address TEXT;
-
-ALTER TABLE items ADD COLUMN created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
 
 -- To get all items from lost items table
-SELECT * FROM lost_found_db.items;
+SELECT * FROM lost_found_db.lost_items;
 
 
 -- Creation of Found items table
-
 CREATE TABLE Found_Items (
     id INT PRIMARY KEY AUTO_INCREMENT,
     item_name VARCHAR(255) NOT NULL,
@@ -47,4 +43,15 @@ ADD COLUMN address TEXT;
 
 -- To get all items from found items table
 SELECT * FROM lost_found_db.found_items;
+
+-- Creation of Users table
+CREATE TABLE users (
+  full_name VARCHAR(255) NOT NULL,
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(255) NOT NULL UNIQUE,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  phone_number VARCHAR(20) NOT NULL,
+  address varchar(255) not null
+);
 
